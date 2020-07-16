97fb0ec4371164f699162368c66b2ce69d57f15a
Wendy Pineda <65093195+Wendy36593@users.noreply.github.com>
2020-07-16 16:51:33 -0600

    gregando primer trabajo en pagina

---
+++ Documents/Wendy/Sitio/css-html-master sabid/css-html-master/Sitio web/Index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en"> 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body id="Website">
+    <p class="parrafos">
+       <span>Hola</span>, Cancion.
+       <span>Hola <strong>Bello</strong> </span>
+    </p>
+    <img src="" alt="Soy una imagen">
+    
+    ?php
+    echo "Aprendiendo a Programar";
+    ?>
+
+
+</body>
+</html>
